e92b809c25839dad2c86e5e303c59a4c26cbce89
Edgar <edvlicup@gmail.com>
2022-02-22 07:09:25 -0800

    added Pass and Person classes, and main base

--- src/java/Person.java
+++ src/java/Person.java
@@ -1,2 +1,62 @@
 public class Person {
+    String name;
+    String email;
+    String phoneNum;
+
+    //Default Constructor
+    public Person(){
+
+    }
+    //Overloaded Constructor
+    public Person(String name, String email, String phoneNum, Character gender, int age) {
+        this.name = name;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    //Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    Character gender;
+    int age;
 }
